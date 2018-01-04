177c8deda19c16d21c7d5630d77c7ded9c55d240
Christian Kim <christiandwkim@gmail.com>
2018-01-04 18:56:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,12 +7,12 @@ var ingredient6 = "garlic"
 
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray('foo', 1) {
+function addElementToBeginningOfArray(numbers) {
   var numbers = [1]
   numbers = ['foo', ...numbers]
 }
 
-function destructivelyAddElementToBeginningOfArray('foo', 1) {
+function destructivelyAddElementToBeginningOfArray(numbers) {
   var numbers = [1]
   numbers.unshift ('foo')
 }
